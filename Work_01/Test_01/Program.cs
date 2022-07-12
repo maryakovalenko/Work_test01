e3115cbcb775c382184575669e4c5eac1fb269dc
maryakovalenko <pomashka18061992@gmail.com>
2022-07-12 16:07:42 +0300

    Program.cs

--- Work_01/Test_01/Program.cs
+++ Work_01/Test_01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача №2");
+Console.WriteLine ("-----------------------------");
+
+Console.Write ("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine ());    
+Console.Write ("Введите второе целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine ());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число  ( " + numberA + ") больше второго (" + numberB + ")");
+}
+else
+{
+    Console.WriteLine("Второе число (" + numberB + ") больше первого (" + numberA + ")"); 
+}
+
