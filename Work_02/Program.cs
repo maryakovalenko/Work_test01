f9c5317971bcad02e4d62ed2dd283176a4f3db75
maryakovalenko <pomashka18061992@gmail.com>
2022-07-12 16:55:05 +0300

    Program.cs

---
+++ Work_02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задача 4");
+
+Console.Write("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if (max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальным из трех чисел (" + numberA + ", " + numberB +"," + numberC + ") является число " + max);
