739af41740796c32a8c475fa635bb97a6feb99dd
maryakovalenko <pomashka18061992@gmail.com>
2022-07-13 11:56:56 +0300

    Program.cs

---
+++ Work_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задача №6");
+
+Console.WriteLine("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.Write("Число" + numberA +" четное");
+}
+else
+{
+    Console.Write("Число" + numberA +" нечетное");
+}
