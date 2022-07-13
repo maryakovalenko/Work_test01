aa6b1e88c6de02b0655b6a046048e299c94fd39c
maryakovalenko <pomashka18061992@gmail.com>
2022-07-13 12:10:39 +0300

    Program.cs

---
+++ Work_04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача №8");
+
+Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int i=1;
+
+while (i<=numberA)
+{
+    if  (i % 2 == 0)
+
+{
+    Console.Write(i + " , ");
+}
+ i++;
+}
